001a56f1b23964c28370cc7545b86cd686ab44c2
Abhishek <abhishek.madepogu@gmail.com>
2020-08-11 13:05:35 -0400

    Cart Edited

--- app/src/main/java/com/abhishek/annadatha/CartActivity.java
+++ app/src/main/java/com/abhishek/annadatha/CartActivity.java
@@ -1,10 +1,13 @@
 package com.abhishek.annadatha;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -20,6 +23,8 @@ import com.abhishek.annadatha.ViewHolder.CartViewHolder;
 import com.abhishek.annadatha.ViewHolder.ProductViewHolder;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -56,12 +61,51 @@ public class CartActivity extends AppCompatActivity {
         FirebaseRecyclerOptions<Cart> options= new FirebaseRecyclerOptions.Builder<Cart>().setQuery(databaseReference.child(Prevalent.CurrentonlineUser.getPhone()),Cart.class).build();
        FirebaseRecyclerAdapter<Cart,CartViewHolder> adapter=new FirebaseRecyclerAdapter<Cart, CartViewHolder>(options) {
            @Override
-           protected void onBindViewHolder(@NonNull CartViewHolder holder, int i, @NonNull Cart cart) {
+           protected void onBindViewHolder(@NonNull CartViewHolder holder, int i, @NonNull final Cart cart) {
               // Toast.makeText(CartActivity.this, "Hello", Toast.LENGTH_SHORT).show();
                holder.txtPrice.setText(cart.getPrice());
                holder.txtQuanity.setText(cart.getQuantity());
               // holder.txtPrice.setText(cart.getPrice());
                holder.txtname.setText(cart.getPname());
+               holder.itemView.setOnClickListener(new View.OnClickListener() {
+                   @Override
+                   public void onClick(View v) {
+                       CharSequence options[]=new CharSequence[]{
+
+                               "Edit",
+                               "Remove"
+
+                       };
+                       AlertDialog.Builder builder=new AlertDialog.Builder(CartActivity.this);
+                       builder.setTitle("Cart Options");
+                       builder.setItems(options, new DialogInterface.OnClickListener() {
+                           @Override
+                           public void onClick(DialogInterface dialog, int which) {
+                               if(which==0){
+                                   //changeProductState(pid);
+                                   Intent i=new Intent(CartActivity.this,ProductDetailsActivity.class);
+                                   i.putExtra("pid",cart.getPid());
+                                   startActivity(i);
+                               }
+                               if(which==1){
+                                   databaseReference.child(Prevalent.CurrentonlineUser.getPhone()).child(cart.getPid()).removeValue().
+                                           addOnCompleteListener(new OnCompleteListener<Void>() {
+                                               @Override
+                                               public void onComplete(@NonNull Task<Void> task) {
+                                                   if(task.isSuccessful()){
+                                                       Toast.makeText(CartActivity.this, "Product Removed Successfully", Toast.LENGTH_SHORT).show();
+                                                   }
+                                                   
+                                               }
+                                           });
+                                   
+                               }
+
+                           }
+                       });
+                       builder.show();
+                   }
+               });
            }
 
            @NonNull
